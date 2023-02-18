ce81be38968388a556d59c10b3faaba14b0a897f
Tomasz Nocoń <tomasz13nocon@gmail.com>
2023-02-18 20:16:09 +0100

    blur working nicely

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,3 +1,25 @@
-// This needs to be one of the tailwind values
-export const IMAGE_VIEWER_TRANSITION = 500;
-export const BLUR_TRANSITION = 500;
+import { atom } from 'nanostores';
+
+export const IMAGE_VIEWER_TRANSITION = 400;
+export const BLUR_TRANSITION = 400;
+
+export const hidingImageViewer = atom(false);
+
+export function hideImageViewer() {
+  hidingImageViewer.set(true);
+  let imageViewer = document.getElementById("image-viewer") as HTMLElement;
+  let blurImgs = document.getElementsByClassName("blur-img") as HTMLCollectionOf<HTMLElement>;
+
+  imageViewer.style.opacity = "0";
+  for (let blurImg of blurImgs) {
+    blurImg.style.opacity = "0";
+  }
+
+  setTimeout(() => {
+    imageViewer.classList.add("hidden")
+    for (let blurImg of blurImgs) {
+      blurImg.classList.add("hidden");
+    }
+    hidingImageViewer.set(false);
+  }, IMAGE_VIEWER_TRANSITION);
+}
