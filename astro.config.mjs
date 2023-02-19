6a657a124d305cf4ea7d3857428f30f0f24bc939
Tomasz Nocoń <tomasz13nocon@gmail.com>
2023-02-19 11:41:14 +0100

    netlify cms test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import NetlifyCMS from 'astro-netlify-cms';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -8,8 +9,31 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  })],
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp'
+    }),
+    NetlifyCMS({
+      config: {
+        backend: {
+          name: 'git-gateway',
+          branch: 'beta',
+        },
+        collections: [
+          {
+            name: "diary",
+            label: "Diary",
+            folder: "src/content/diary",
+            create: true,
+            fields: [
+              { name: 'title', widget: 'string', label: 'Post Title' },
+              { name: 'body', widget: 'markdown', label: 'Post Body' },
+            ],
+          }
+        ],
+      },
+    }),
+  ],
   server: { host: true },
 });
--- src/content/diary/jdlask.md
+++ src/content/diary/jdlask.md
@@ -1,5 +1,18 @@
 ---
-title: "Another post"
+title: Another post
 date: 2023-01-07
 description: ""
 ---
+hello *this* is a **post**
+
+here's a list:
+
+* one
+* two
+* three
+
+## more text
+
+![guy and mountains](/falaq-lazuardi-ey7d79nspiy-unsplash.jpg)
+
+image too large?
