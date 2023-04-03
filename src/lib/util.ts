912e3e1c1c50427e397ab973b91ce6acade88f88
tomasz13nocon <tomasz13nocon@gmail.com>
2023-04-03 18:11:58 +0200

    photography progress, before fancy link removal

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -6,10 +6,16 @@ export const IMAGE_TRANSITION = 400;
 
 export const hidingImageViewer = atom(false);
 
+// Dybamically import an image from /src/assets
+// Path has to start with /src/assets
 // Throws if `path` doesn't exist in /src/assets
 export async function getAsset(path: string) {
   const images = import.meta.glob("/src/assets/**/*");
   return images[path]() as Promise<{ default: ImageMetadata }>;
+}
+
+export function toUrl(str: string) {
+  return encodeURIComponent(str.toLowerCase().replaceAll(" ", "-"));
 }
 
 export function hideImageViewer() {
