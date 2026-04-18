998df31fc3fee389fc1567d58a3ccff557994345
tomasz13nocon <tomasz13nocon@gmail.com>
2026-04-18 13:25:11 +0200

    upgrade deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,22 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig, fontProviders } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [icon()],
   devToolbar: { enabled: false },
   server: {
     host: true,
   },
   vite: {
-    plugins: [],
+    plugins: [tailwindcss()],
   },
+  fonts: [
+    {
+      name: "Open Sans",
+      cssVariable: "--font-open-sans",
+      provider: fontProviders.fontsource(),
+      weights: [400, 500, 700],
+    },
+  ],
 });
